88e8ae6130e9dd045f8d1857db57b5c04d1bec59
valtteri <valtterih@icloud.com>
2021-03-27 20:48:50 +0200

    Only missing keydown indicator on screen

--- synthscript.js
+++ synthscript.js
@@ -1,5 +1,16 @@
 let keys = document.querySelectorAll(".key");
+let keyCodes = [
+    {code:"90",key:"z",note:"C"},
+    {code:"88",key:"x",note:"D"},
+    {code:"67",key:"c",note:"E"},
+    {code:"86",key:"v",note:"F"},
+    {code:"66",key:"b",note:"G"},
+    {code:"78",key:"n",note:"A"},
+    {code:"77",key:"m",note:"B"},
+];
 
+let whiteKeys =     ["90","83","88","68","67","86","71","66","72","78","74","77"];
+let whiteSounds =   ["C" ,"Db","D" ,"Eb","E" ,"F" ,"Gb","G" ,"Ab","A" ,"Bb","B"];
 
 keys.forEach((note) => {
     note.addEventListener("mousedown",synthPress);
@@ -9,6 +20,7 @@ function synthPress(){
     let soundOBject = new Audio("sounds/"+this.id+".mp3");
     soundOBject.loop = true;
     soundOBject.play();
+
     keys.forEach((note) => {
         note.addEventListener("mouseup", () => {
             soundOBject.pause();
@@ -16,14 +28,25 @@ function synthPress(){
     });
 };
 
-document.body.addEventListener("keydown",testFunk);
-
-
-function testFunk(){
+document.body.addEventListener("keydown", (e) => {
+    if (e.repeat) return;
     let pressed = event.keyCode;
+    let foundya = whiteKeys.indexOf(`${pressed}`);
+    console.log("wtf "+whiteKeys.indexOf(`${pressed}`));
+        let keySound = new Audio("sounds/"+whiteSounds[foundya]+".mp3");
+        keySound.loop = true;
+        keySound.play();
+
+        document.body.addEventListener("keyup", () => {
+            keySound.pause();
+        });
     console.log("I pressed the key "+pressed);
-    document.body.addEventListener("keyup",testFun);
-}
+    
+        document.body.addEventListener("keyup",testFun);
+});
+
+    
+
 
 function testFun(){
     let pressed = event.keyCode;
