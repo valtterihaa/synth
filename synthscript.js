df3e7d63c97810eaf05b2d6b96bea15117d0324a
valtteri <valtterih@icloud.com>
2021-03-27 21:12:50 +0200

    working wonkily still

--- synthscript.js
+++ synthscript.js
@@ -1,16 +1,6 @@
-let keys = document.querySelectorAll(".key");
-let keyCodes = [
-    {code:"90",key:"z",note:"C"},
-    {code:"88",key:"x",note:"D"},
-    {code:"67",key:"c",note:"E"},
-    {code:"86",key:"v",note:"F"},
-    {code:"66",key:"b",note:"G"},
-    {code:"78",key:"n",note:"A"},
-    {code:"77",key:"m",note:"B"},
-];
-
-let whiteKeys =     ["90","83","88","68","67","86","71","66","72","78","74","77"];
-let whiteSounds =   ["C" ,"Db","D" ,"Eb","E" ,"F" ,"Gb","G" ,"Ab","A" ,"Bb","B"];
+const keys = document.querySelectorAll(".key");
+const whiteKeys =     ["90","83","88","68","67","86","71","66","72","78","74","77"];
+const whiteSounds =   ["C" ,"Db","D" ,"Eb","E" ,"F" ,"Gb","G" ,"Ab","A" ,"Bb","B"];
 
 keys.forEach((note) => {
     note.addEventListener("mousedown",synthPress);
@@ -32,6 +22,9 @@ document.body.addEventListener("keydown", (e) => {
     if (e.repeat) return;
     let pressed = event.keyCode;
     let foundya = whiteKeys.indexOf(`${pressed}`);
+    // let activeTo = document.getElementById(`whiteSounds[foundya]`);
+    // console.log("this is"+activeTo);
+    // activeTo.classList.add("active");
     console.log("wtf "+whiteKeys.indexOf(`${pressed}`));
         let keySound = new Audio("sounds/"+whiteSounds[foundya]+".mp3");
         keySound.loop = true;
@@ -39,16 +32,8 @@ document.body.addEventListener("keydown", (e) => {
 
         document.body.addEventListener("keyup", () => {
             keySound.pause();
+            // activeTo.classList.remove("active");
         });
     console.log("I pressed the key "+pressed);
     
-        document.body.addEventListener("keyup",testFun);
 });
-
-    
-
-
-function testFun(){
-    let pressed = event.keyCode;
-    console.log("I stopped pressing the key "+pressed);
-}
