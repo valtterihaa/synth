436b19fce0052bbb0e8b0af89d051539664a1cbd
valtteri <valtterih@icloud.com>
2021-08-13 11:19:26 +0300

    opus'd audios

--- synthscript.js
+++ synthscript.js
@@ -1,34 +1,41 @@
 const keys = document.querySelectorAll(".key");
-const allKeys =     ["90","83","88","68","67","86","71","66","72","78","74","77"];
-const sounds =   ["C" ,"Db","D" ,"Eb","E" ,"F" ,"Gb","G" ,"Ab","A" ,"Bb","B"];
+const keyNumbers =      ["90","83","88","68","67","86","71","66","72","78","74","77"];
+const keyBoardKeys =    ["z","s","x","d","c","v","g","b","h","n","j","m"]
+const sounds =          ["C" ,"Db","D" ,"Eb","E" ,"F" ,"Gb","G" ,"Ab","A" ,"Bb","B"];
 
 keys.forEach((note) => {
-    note.addEventListener("mousedown",synthPress);
+    note.addEventListener("mousedown",playAudio)
+    note.addEventListener("mouseup",stopAudio)
 });
 
-function synthPress(){
-    let soundOBject = new Audio("sounds/"+this.id+".mp3");
-    soundOBject.loop = true;
-    soundOBject.play();
-    keys.forEach((note) => {
-        note.addEventListener("mouseup", () => {
-            soundOBject.pause();
-        });
-    });
-};
+document.body.addEventListener("keydown", e => {
+    if (e.repeat) return
+    if (!keyBoardKeys.includes(e.key)) return
+    let keyIndex = keyBoardKeys.indexOf(e.key)
+    let audioElement = document.getElementById(`${sounds[keyIndex].toUpperCase()}.opus`)
+    let highLightElement = document.getElementById(`${sounds[keyIndex].toUpperCase()}`)
+    highLightElement.classList.add("active")
+    console.log(e.key.toUpperCase())
+    audioElement.play()
+    document.body.addEventListener("keyup", e => {
+        if (e.repeat) return
+        if (!keyBoardKeys.includes(e.key)) return
+        let stopKeyIndex = keyBoardKeys.indexOf(e.key)
+        let stopAudioElement = document.getElementById(`${sounds[stopKeyIndex].toUpperCase()}.opus`)
+        let unHighLightElement = document.getElementById(`${sounds[stopKeyIndex].toUpperCase()}`)
+        unHighLightElement.classList.remove("active")
+        stopAudioElement.pause()
+    })
+})
 
-document.body.addEventListener("keydown", (e) => {
-    if (e.repeat) return;
-    let pressed = event.keyCode;
-    let foundya = allKeys.indexOf(`${pressed}`);
-    let note = sounds[foundya];
-    let activeTo = document.getElementById(note);
-    activeTo.classList.add("active");
-        let keySound = new Audio("sounds/"+note+".mp3");
-        keySound.loop = true;
-        keySound.play();
-        document.body.addEventListener("keyup", () => {
-            keySound.pause();
-            activeTo.classList.remove("active");
-        });    
-});
+function playAudio(){
+    let audio = document.getElementById(this.id+".opus")
+    audio.loop = true
+    audio.play()
+}
+
+function stopAudio(){
+    console.log(this.id)
+    let audio = document.getElementById(this.id+".opus")
+    audio.pause()
+}
